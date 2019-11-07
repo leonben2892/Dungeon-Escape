7f2401a9cd8081abb1ba65e6d5e88748c360c27c
Leon <leon.ben2892@gmail.com>
2019-11-07 22:22:50 +0200

    Added moss giant enemy movement & animation

---
+++ Assets/Dungeon Escape/Scripts/Enemy/MossGiant.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MossGiant : Enemy
+{
+    private Vector3 currentTarget;
+    private Animator anim;
+    private SpriteRenderer mossSprite;
+
+    private void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+        mossSprite = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    public override void Update()
+    {
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+        {
+            return;
+        }
+        Movement();
+    }
+
+    void Movement()
+    {
+        Flip();
+
+        if (transform.position == pointA.position)
+        {
+            currentTarget = pointB.position;
+            anim.SetTrigger("Idle");
+        }
+        else if (transform.position == pointB.position)
+        {
+            currentTarget = pointA.position;
+            anim.SetTrigger("Idle");
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, currentTarget, speed * Time.deltaTime);
+    }
+
+    void Flip()
+    {
+        if (currentTarget == pointA.position)
+        {
+            mossSprite.flipX = true;
+        }
+        else
+        {
+            mossSprite.flipX = false;
+        }
+    }
+
+}
