49a2c874e50694b26154a9d7496f07818a030630
Leon <leon.ben2892@gmail.com>
2019-11-08 00:48:50 +0200

    Added spider enemy movement & animation

--- Assets/Dungeon Escape/Scripts/Enemy/Spider.cs
+++ Assets/Dungeon Escape/Scripts/Enemy/Spider.cs
@@ -4,19 +4,52 @@ using UnityEngine;
 
 public class Spider : Enemy
 {
+    private Vector3 currentTarget;
+    private Animator anim;
+    private SpriteRenderer spiderSprite;
+
     private void Start()
     {
-        Attack();
-    }
-
-    public override void Attack()
-    {
-        base.Attack();
-        Debug.Log("Spider Attack!");
+        anim = GetComponentInChildren<Animator>();
+        spiderSprite = GetComponentInChildren<SpriteRenderer>();
     }
 
     public override void Update()
     {
-        
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+        {
+            return;
+        }
+        Movement();
+    }
+
+    void Movement()
+    {
+        Flip();
+
+        if (transform.position == pointA.position)
+        {
+            currentTarget = pointB.position;
+            anim.SetTrigger("Idle");
+        }
+        else if (transform.position == pointB.position)
+        {
+            currentTarget = pointA.position;
+            anim.SetTrigger("Idle");
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, currentTarget, speed * Time.deltaTime);
+    }
+
+    void Flip()
+    {
+        if(currentTarget == pointA.position)
+        {
+            spiderSprite.flipX = true;
+        }
+        else
+        {
+            spiderSprite.flipX = false;
+        }
     }
 }
